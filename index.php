4460db7b7b80b7d214f945cf8d741687fca897f8
Caio Matheus dos Santos Abra <59180237+CaioAbra@users.noreply.github.com>
2021-02-07 16:29:14 -0300

    navbar top

--- index.php
+++ index.php
@@ -25,6 +25,60 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gêneros</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Lista de animes
+                        </a>
+
+                        <ul class="dropdown-menu text-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Legendado</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Dublado</a></li>
+                        </ul>
+
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="temporada" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Temporadas
+                        </a>
+
+                        <ul class="dropdown-menu text-end" aria-labelledby="temporada">
+                            <li><a class="dropdown-item" href="#">Atual</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Todas</a></li>
+                        </ul>
+
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -36,7 +90,9 @@
     </div>
 
     <script src="public/lib/jquery-3.5.1.js"></script>
-    <script src="public/lib/popper/popper.js"></script> 
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- <script src="public/lib/popper/popper.js"></script>  -->
     <script src="public/js/index.js"></script>
     <!-- boostrap -->
     <script src="public/lib/bootstrap-5.0.0-beta/js/bootstrap.bundle.min.js"></script>
--- login.php
+++ login.php
@@ -79,7 +79,9 @@
     </div>
 
     <script src="public/lib/jquery-3.5.1.js"></script>
-    <script src="public/lib/popper/popper.js"></script> 
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- <script src="public/lib/popper/popper.js"></script>  -->
     <!-- <script src="public/lib/popper/popper.min.js.map"></script>  -->
     <script src="public/js/index.js"></script>
     <!-- boostrap -->
