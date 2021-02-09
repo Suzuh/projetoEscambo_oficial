98ec0f16a5007122afdfcacef060e9d5c782dce4
João Gabriel dos Anjos Leal <70417853+Suzuh@users.noreply.github.com>
2021-02-08 22:14:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
             <a class="navbar-brand" href="index.php">
-                <img src="public/img/logo_purple.png" alt="logo da escambo" width="190" height="80">
+                <img src="public/img/logo_purple.png" alt="logo da escambo" width="220" height="80">
             </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
